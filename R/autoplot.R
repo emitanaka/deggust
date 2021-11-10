edc6dafa6f2a2fcfa85f4ba2da1e80a15b1d1b6f
Emi <dr.emi.tanaka@gmail.com>
2021-11-11 07:41:46 +1100

    updated to fix functions removed from edibble major overhaul

--- R/autoplot.R
+++ R/autoplot.R
@@ -11,8 +11,8 @@
 #' @param .edibble An edibble desgin, an edibble table or an edibble graph.
 #' @param view A string specifying the level of the view ("high" or "low") to plot.
 #'  By default it is "high".
-#' @param ... Additional parameters passed to `plot.igraph`.
-#' @param main The title of the plot. By default it is the
+#' @param ... Unused at the moment.
+#' @param title The title of the plot. By default it is the
 #'  name of the edibble design.
 #' @name autoplot.edibble
 #' @return A `ggraph` and `ggplot` object.
@@ -20,57 +20,53 @@
 #'
 NULL
 
-#' @importFrom ggplot2 autoplot
-#' @rdname autoplot.edibble
-#' @export
-autoplot.EdibbleDesign <- function(.edibble, view = c("high", "low"),
-                                   ..., main = NULL) {
-  main <- main %||% .edibble$name
-  .graph <- .edibble$graph
-  autoplot(.graph, view = view, ..., main = main)
-}
+#' #' @importFrom ggplot2 autoplot
+#' #' @rdname autoplot.edibble
+#' #' @export
+#' autoplot.edbl_design <- function(.edibble, view = c("high", "low"),
+#'                                    ..., title = NULL) {
+#'   title <- title %||% .edibble$name
+#'   .graph <- .edibble$design
+#'   autoplot(.graph, view = view, ..., main = title)
+#' }
+#'
+#' #' @rdname autoplot.edibble
+#' #' @importFrom edibble subset_vars subset_levels
+#' #' @importFrom ggplot2 aes scale_color_manual ggtitle
+#' #' @importFrom ggraph ggraph label_rect geom_edge_link geom_node_point circle geom_node_text
+#' #' @importFrom grid arrow unit
+#' #' @export
+#' autoplot.edbl_graph <- function(.edibble, view = c("high", "low"),
+#'                                 ..., main = NULL) {
+#'   main <- main %||% "An edibble design"
+#'   view <- match.arg(view)
+#'   out <- switch(view,
+#'                 high = subset_vars(.edibble),
+#'                 low = subset_levels(.edibble))
+#'   g <- ggraph(out) +
+#'     geom_edge_link(arrow = arrow(length = unit(4, 'mm')),
+#'                    aes(start_cap = label_rect(node1.name),
+#'                        end_cap = label_rect(node2.name),
+#'                        ## this is fine when loading
+#'                        ## but get object "etype" not found error
+#'                        # linetype = edge_ltype_dict[etype]
+#'                        )) +
+#'     geom_node_text(aes(label = label, color = class)) +
+#'     scale_color_manual(name = "Variable Type",
+#'                        breaks = c("edbl_trt", "edbl_unit", "edbl_rcrd"),
+#'                        labels = c("Treatment", "Unit", "Record"),
+#'                        values = c(deggust_opt("vertex.label.color.unit"),
+#'                                   deggust_opt("vertex.label.color.trt"),
+#'                                   deggust_opt("vertex.label.color.rcrd"))) +
+#'     ggtitle(main)
+#'   g
+#' }
 
 #' @rdname autoplot.edibble
-#' @importFrom edibble subset_vars subset_levels
-#' @importFrom ggplot2 aes scale_color_manual ggtitle
-#' @importFrom ggraph ggraph label_rect geom_edge_link geom_node_point circle geom_node_text
-#' @importFrom grid arrow unit
-#' @export
-autoplot.edbl_graph <- function(.edibble, view = c("high", "low"),
-                                ..., main = NULL) {
-  main <- main %||% "An edibble design"
-  view <- match.arg(view)
-  out <- switch(view,
-                high = subset_vars(.edibble),
-                low = subset_levels(.edibble))
-  g <- ggraph(out) +
-    geom_edge_link(arrow = arrow(length = unit(4, 'mm')),
-                   aes(start_cap = label_rect(node1.name),
-                       end_cap = label_rect(node2.name),
-                       ## this is fine when loading
-                       ## but get object "etype" not found error
-                       # linetype = edge_ltype_dict[etype]
-                       )) +
-    geom_node_text(aes(label = label, color = class)) +
-    scale_color_manual(name = "Variable Type",
-                       breaks = c("edbl_trt", "edbl_unit", "edbl_rcrd"),
-                       labels = c("Treatment", "Unit", "Record"),
-                       values = c(deggust_opt("vertex.label.color.unit"),
-                                  deggust_opt("vertex.label.color.trt"),
-                                  deggust_opt("vertex.label.color.rcrd"))) +
-    ggtitle(main)
-  g
-}
-
-#' @rdname autoplot.edibble
-#' @importFrom edibble is_edibble get_edibble_design
 #' @export
 autoplot.edbl_table <- function(.edibble, aspect_ratio = 1,
                                 shape = "circle", text = FALSE, image = NULL,
                                 trts = NULL, scales = NULL) {
-  if(!is_edibble(.edibble)) {
-    abort("Don't know how to plot an edibble table with no design.")
-  }
 
   ind_units <- unlist(lapply(.edibble, is_edibble_unit))
   unit_names <- names(ind_units)[ind_units]
@@ -126,7 +122,8 @@ autoplot.edbl_table <- function(.edibble, aspect_ratio = 1,
     }
   } else if(nunits==2 & ntrts==1) {
     # make it two-dimensional
-    unames <- unit_names(.edibble)
+    ind_units <- unlist(lapply(.edibble, is_edibble_unit))
+    unames <- names(ind_units)[ind_units]
     nlevels_units <- unlist(lapply(.edibble[unames], nlevels))
     unit_name <- unames[which.max(nlevels_units)]
     parent_name <- setdiff(unames, unit_name)
@@ -136,7 +133,9 @@ autoplot.edbl_table <- function(.edibble, aspect_ratio = 1,
     nodes <- as_data_frame(nodes)
 
     tt <- vapply(split(nodes, nodes[unames[1]]), nrow, numeric(1))
-    nodes$xnames <- names_to_nesting_names(.edibble, paste0(unames[2], ":", nodes[[unames[2]]]))
+    SDF <- split(.edibble[[unames[2]]], .edibble[[unames[1]]])
+    LDF <- unlist(lapply(SDF, function(x) paste0("U", 1:length(x))))
+    nodes$xnames <- unname(LDF)
 
     #if(max(tt) <= w) {
       nodes$x <- as.numeric(as.factor(nodes$xnames))
