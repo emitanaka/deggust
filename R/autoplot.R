2bcbf157f7592d8d08aacbeafe0f2cf069b5fa35
Emi <dr.emi.tanaka@gmail.com>
2022-01-17 00:15:37 +1100

    LSD autoplot

--- R/autoplot.R
+++ R/autoplot.R
@@ -3,66 +3,12 @@
 
 #' Auto plot or `ggplot2` of an edibble design
 #'
-#' @description
-#'
-#' This plot produces the same type of plot using the `plot` functions in
-#' `edibble` package except the plots are `ggplot`
 #'
 #' @param .edibble An edibble desgin, an edibble table or an edibble graph.
-#' @param view A string specifying the level of the view ("high" or "low") to plot.
-#'  By default it is "high".
 #' @param ... Unused at the moment.
 #' @param title The title of the plot. By default it is the
 #'  name of the edibble design.
-#' @name autoplot.edibble
-#' @return A `ggraph` and `ggplot` object.
-#' @seealso See \code{\link[edibble]{plot.edibble}} for the base plot version of this plot.
-#'
-NULL
-
-#' #' @importFrom ggplot2 autoplot
-#' #' @rdname autoplot.edibble
-#' #' @export
-#' autoplot.edbl_design <- function(.edibble, view = c("high", "low"),
-#'                                    ..., title = NULL) {
-#'   title <- title %||% .edibble$name
-#'   .graph <- .edibble$design
-#'   autoplot(.graph, view = view, ..., main = title)
-#' }
-#'
-#' #' @rdname autoplot.edibble
-#' #' @importFrom edibble subset_vars subset_levels
-#' #' @importFrom ggplot2 aes scale_color_manual ggtitle
-#' #' @importFrom ggraph ggraph label_rect geom_edge_link geom_node_point circle geom_node_text
-#' #' @importFrom grid arrow unit
-#' #' @export
-#' autoplot.edbl_graph <- function(.edibble, view = c("high", "low"),
-#'                                 ..., main = NULL) {
-#'   main <- main %||% "An edibble design"
-#'   view <- match.arg(view)
-#'   out <- switch(view,
-#'                 high = subset_vars(.edibble),
-#'                 low = subset_levels(.edibble))
-#'   g <- ggraph(out) +
-#'     geom_edge_link(arrow = arrow(length = unit(4, 'mm')),
-#'                    aes(start_cap = label_rect(node1.name),
-#'                        end_cap = label_rect(node2.name),
-#'                        ## this is fine when loading
-#'                        ## but get object "etype" not found error
-#'                        # linetype = edge_ltype_dict[etype]
-#'                        )) +
-#'     geom_node_text(aes(label = label, color = class)) +
-#'     scale_color_manual(name = "Variable Type",
-#'                        breaks = c("edbl_trt", "edbl_unit", "edbl_rcrd"),
-#'                        labels = c("Treatment", "Unit", "Record"),
-#'                        values = c(deggust_opt("vertex.label.color.unit"),
-#'                                   deggust_opt("vertex.label.color.trt"),
-#'                                   deggust_opt("vertex.label.color.rcrd"))) +
-#'     ggtitle(main)
-#'   g
-#' }
-
-#' @rdname autoplot.edibble
+#' @return A `ggplot` object.
 #' @export
 autoplot.edbl_table <- function(.edibble, aspect_ratio = 1,
                                 shape = "circle", text = FALSE, image = NULL,
@@ -80,93 +26,127 @@ autoplot.edbl_table <- function(.edibble, aspect_ratio = 1,
 
   nlevels_unit <- nrow(.edibble)
 
+  des <- edbl_design(.edibble)
+  obsid <- fct_obs_unit(des)
+  parentids <- intersect(fct_parent(des, obsid), unit_ids(des))
+
   if(nunits==1) {
-    # make it snake-like
-    unit_vec <- .edibble[[unit_names]]
-    nodes <- .edibble[c(unit_names, setdiff(names(.edibble), unit_names))]
-    nodes <- as_data_frame(nodes)
-    nodes <- cbind(nodes, coord_snake(nlevels_unit, aspect_ratio))
-    plot <- ggplot(nodes, aes(x, y)) +
-      geom_path()
-    if(is.null(image)) {
-        plot <- plot + geom_unit_node(aes(fill = !!parse_expr(trt_names[1])), shape = shape)
-    } else {
-      if(!require("ggimage")) {
-        stop("Please install `ggimage` package to use the image argument.")
-      }
-      plot <- plot + ggimage::geom_image(aes(color = !!parse_expr(trt_names[1])), image = image, size = 0.1)
-    }
-    if(ntrts > 1) {
-      plot <- another_fill_scale(plot, 2, scales, trt_names, shape = shape, image = image,
-                                 height = 0.6, width = 0.6, size = 0.08)
-      if(ntrts > 2) {
-        plot <- another_fill_scale(plot, 3, scales, trt_names, shape = shape, image = image,
-                                   height = 0.4, width = 0.4, size = 0.05)
-      }
-    }
-
-    plot <- plot +
-      theme(axis.ticks.length = grid::unit(0, "npc"),
-            panel.grid = element_blank(),
-            axis.title = element_blank(),
-            axis.text = element_blank())
-    if(isTRUE(text) || inherits(text, "element_text")) {
-      text_aes <- list()
-      if(inherits(text, "element_text")) {
-        text_aes <- remove_nulls(as.list(text))
-        names(text_aes) <- gsub("face", "fontface", names(text_aes))
-      }
-      plot <- plot +
-        do.call("geom_text", c(list(mapping = aes(label = unit_vec)),
-                               text_aes))
-    }
+    plot <- plot_single_unit(.edibble, nlevels_unit, ntrts, unit_names, trt_names, shape, image, text, aspect_ratio)
   } else if(nunits==2 & ntrts==1) {
-    # make it two-dimensional
-    ind_units <- unlist(lapply(.edibble, is_edibble_unit))
-    unames <- names(ind_units)[ind_units]
-    nlevels_units <- unlist(lapply(.edibble[unames], nlevels))
-    unit_name <- unames[which.max(nlevels_units)]
-    parent_name <- setdiff(unames, unit_name)
-    unit_vec <- .edibble[[unit_name]]
+    plot <- plot_two_units(.edibble, nlevels_unit, ntrts, unit_names, trt_names, shape, image, text, aspect_ratio)
+  } else if(nunits==3 & length(parentids)==2 & ntrts==1) {
+    plot <- plot_three_units(.edibble, nlevels_unit, ntrts, unit_names, trt_names, shape, image, text, aspect_ratio,
+                             obsid, parentids)
+  } else {
+    abort("`autoplot` is not yet supported for this design.")
+  }
+  plot
+}
 
-    nodes <- .edibble[c(unit_names, setdiff(names(.edibble), unit_names))]
-    nodes <- as_data_frame(nodes)
+plot_three_units <- function(.edibble, nlevels_unit, ntrts, unit_names, trt_names, shape, image, text, aspect_ratio,
+                           obsid, parentids) {
+  des <- edbl_design(.edibble)
+  vlevs <- fct_levels(des)
+  parent_labels <- fct_label(des, parentids)
+  parent1 <- parent_labels[which.max(lengths(vlevs[parent_labels]))]
+  parent2 <- setdiff(parent_labels, parent1)
 
-    tt <- vapply(split(nodes, nodes[unames[1]]), nrow, numeric(1))
-    SDF <- split(.edibble[[unames[2]]], .edibble[[unames[1]]])
-    LDF <- unlist(lapply(SDF, function(x) paste0("U", 1:length(x))))
-    nodes$xnames <- unname(LDF)
+  ggplot(.edibble, aes(!!parse_expr(parent1), !!parse_expr(parent2), fill = !!parse_expr(trt_names))) +
+    geom_tile(color = "black", size = 2) +
+    theme(axis.ticks.length = grid::unit(0, "npc"),
+          panel.grid = element_blank())
 
-    #if(max(tt) <= w) {
-      nodes$x <- as.numeric(as.factor(nodes$xnames))
-      nodes$y <- as.numeric(factor(nodes[[parent_name]],
-                                   levels = unique(nodes[[parent_name]])))
-      block_data <- data.frame(x = (max(nodes$x) + 1)/2,
-                               y =  (1:max(nodes$y)),
-                               height = 1.6)
-    #} else {
 
-    #}
+}
 
-    plot <- ggplot(nodes, aes(x = x, y = y)) +
-      geom_unit_node(data = block_data,
-                     shape = "box", aes(width = max(nodes$x) * 2, height = height)) +
-      geom_path(aes(group = !!parse_expr(parent_name))) +
-      geom_unit_node(aes(fill = !!parse_expr(trt_names)), shape = shape) +
-      theme(axis.ticks.length = grid::unit(0, "npc"),
-            panel.grid = element_blank(),
-            axis.title = element_blank(),
-            axis.text = element_blank())
-    if(isTRUE(text) || inherits(text, "element_text")) {
-      text_aes <- list()
-      if(inherits(text, "element_text")) {
-        text_aes <- remove_nulls(as.list(text))
-        names(text_aes) <- gsub("face", "fontface", names(text_aes))
-      }
-      plot <- plot +
-        do.call("geom_text", c(list(mapping = aes(label = unit_vec)),
-                               text_aes))
+plot_two_units <- function(.edibble, nlevels_unit, ntrts, unit_names, trt_names, shape, image, text, aspect_ratio) {
+  # make it two-dimensional
+  ind_units <- unlist(lapply(.edibble, is_edibble_unit))
+  unames <- names(ind_units)[ind_units]
+  nlevels_units <- unlist(lapply(.edibble[unames], nlevels))
+  unit_name <- unames[which.max(nlevels_units)]
+  parent_name <- setdiff(unames, unit_name)
+  unit_vec <- .edibble[[unit_name]]
+
+  nodes <- .edibble[c(unit_names, setdiff(names(.edibble), unit_names))]
+  nodes <- as_data_frame(nodes)
+
+  tt <- vapply(split(nodes, nodes[unames[1]]), nrow, numeric(1))
+  SDF <- split(.edibble[[unames[2]]], .edibble[[unames[1]]])
+  LDF <- unlist(lapply(SDF, function(x) paste0("U", 1:length(x))))
+  nodes$xnames <- unname(LDF)
+
+  #if(max(tt) <= w) {
+  nodes$x <- as.numeric(as.factor(nodes$xnames))
+  nodes$y <- as.numeric(factor(nodes[[parent_name]],
+                               levels = unique(nodes[[parent_name]])))
+  block_data <- data.frame(x = (max(nodes$x) + 1)/2,
+                           y =  (1:max(nodes$y)),
+                           height = 1.6)
+  #} else {
+
+  #}
+
+  plot <- ggplot(nodes, aes(x = x, y = y)) +
+    geom_unit_node(data = block_data,
+                   shape = "box", aes(width = max(nodes$x) * 2, height = height)) +
+    geom_path(aes(group = !!parse_expr(parent_name))) +
+    geom_unit_node(aes(fill = !!parse_expr(trt_names)), shape = shape) +
+    theme(axis.ticks.length = grid::unit(0, "npc"),
+          panel.grid = element_blank(),
+          axis.title = element_blank(),
+          axis.text = element_blank())
+  if(isTRUE(text) || inherits(text, "element_text")) {
+    text_aes <- list()
+    if(inherits(text, "element_text")) {
+      text_aes <- remove_nulls(as.list(text))
+      names(text_aes) <- gsub("face", "fontface", names(text_aes))
     }
+    plot <- plot +
+      do.call("geom_text", c(list(mapping = aes(label = unit_vec)),
+                             text_aes))
+  }
+  plot
+}
+
+plot_single_unit <- function(.edibble, nlevels_unit, ntrts, unit_names, trt_names, shape, image, text, aspect_ratio) {
+  # make it snake-like
+  unit_vec <- .edibble[[unit_names]]
+  nodes <- .edibble[c(unit_names, setdiff(names(.edibble), unit_names))]
+  nodes <- cbind(nodes, coord_snake(nlevels_unit, aspect_ratio))
+  plot <- ggplot(nodes, aes(x, y)) +
+    geom_path()
+  if(is.null(image)) {
+    plot <- plot + geom_unit_node(aes(fill = !!parse_expr(trt_names[1])), shape = shape)
+  } else {
+    if(!require("ggimage")) {
+      stop("Please install `ggimage` package to use the image argument.")
+    }
+    plot <- plot + ggimage::geom_image(aes(color = !!parse_expr(trt_names[1])), image = image, size = 0.1)
+  }
+  if(ntrts > 1) {
+    plot <- another_fill_scale(plot, 2, scales, trt_names, shape = shape, image = image,
+                               height = 0.6, width = 0.6, size = 0.08)
+    if(ntrts > 2) {
+      plot <- another_fill_scale(plot, 3, scales, trt_names, shape = shape, image = image,
+                                 height = 0.4, width = 0.4, size = 0.05)
+    }
+  }
+
+  plot <- plot +
+    theme(axis.ticks.length = grid::unit(0, "npc"),
+          panel.grid = element_blank(),
+          axis.title = element_blank(),
+          axis.text = element_blank())
+  if(isTRUE(text) || inherits(text, "element_text")) {
+    text_aes <- list()
+    if(inherits(text, "element_text")) {
+      text_aes <- remove_nulls(as.list(text))
+      names(text_aes) <- gsub("face", "fontface", names(text_aes))
+    }
+    plot <- plot +
+      do.call("geom_text", c(list(mapping = aes(label = unit_vec)),
+                             text_aes))
   }
   plot
 }
--- tests/testthat/test-autoplot.R
+++ tests/testthat/test-autoplot.R
@@ -1,16 +1,16 @@
 test_that("autoplot", {
 
   test_that("autoplot graph of edibble variables", {
-    des <- edibble::start_design("An awesome design") %>%
-      edibble::set_units(mainplot = 4,
-                         subplot = edibble::nested_in(mainplot, 2))
+    crd <- make_classical("crd", .output = FALSE)
+    autoplot(crd)
 
-    expect_doppelganger("plot design: low-view", autoplot(des, view = "low"))
-    expect_doppelganger("plot design: high-view", autoplot(des, view = "high"))
-    expect_doppelganger("plot graph: low-view", autoplot(des$graph, view = "low"))
-    expect_doppelganger("plot graph: high-view", autoplot(des$graph, view = "high"))
-    expect_doppelganger("plot table: low-view", autoplot(serve_table(des)), view = "low")
-    expect_doppelganger("plot table: high-view", autoplot(serve_table(des)), view = "high")
+    rcbd <- make_classical("rcbd", .output = FALSE)
+    autoplot(rcbd)
+
+    lsd <- make_classical("lsd", .output = FALSE)
+    autoplot(lsd)
+
+
 
   })
 
